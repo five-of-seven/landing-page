a7897b8e875f0a3f1725a1cab7d80419ecd69b54
Lukas Stateczny <statecznylukas@gmail.com>
2018-09-06 20:39:22 +0000

    adding home page refactoring landing page

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,44 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
+import Landing_Page from '../Landing_Page/index'
+import HomePage from '.././HomePage/HomePage'
 
 // React Routing
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 // *************
 
 // Custom CSS styling
 import './App.css';
+import './youtube_iframe_api/index.css'
 // ******************
 
-import TopBar from '../Landing-page-TopBar/index.js';
-import LogIn from '../LogIn/index';
-
 class App extends Component {
+
+  componentDidMount () {
+    const script = document.createElement("script");
+
+    script.src = "./youtube_iframe_api/index.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   render() {
     return (
-      <div className="App" >
-      {/* <Grid> */}
-      <TopBar/>
-      <LogIn/>
-      </div>
+      <Router>
+        <div>
+
+          <div className="App" >
+          {/* <div className="player"></div> */}
+          {/* <TopBar/>
+          <LogIn/> */}
+          </div>
+
+          <Route exact path="/" component={Landing_Page} />
+          <Route path="/homepage" component={HomePage} />
+
+        </div>
+      </Router>
     );
   }
 }
--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
+Enzyme.configure({ adapter: new EnzymeAdapter() })
+
+test('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
-});
+})
+
+test('should have a one parent div', () => {
+  const wrapper = shallow(<App />)
+  console.log(wrapper.debug())
+})
